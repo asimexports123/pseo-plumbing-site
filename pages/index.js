ce5142722b823ba8cd6f03958ba063c0d8f439cf
asimexports123 <asimexports123@gmail.com>
2026-06-16 08:53:07 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Apex Plumbing</h1>
+      <p>We provide professional plumbing services. Please use a valid city URL.</p>
+    </div>
+  );
+}
